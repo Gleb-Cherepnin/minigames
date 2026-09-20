2bdb178481cc432b640fe4f8fe1d42f2e0f13161
Gleb-Cherepnin <gglebasemil@gmail.com>
2026-09-20 21:03:32 +0500

    refactor: set production base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: mode === 'production' ? '/minigames/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
